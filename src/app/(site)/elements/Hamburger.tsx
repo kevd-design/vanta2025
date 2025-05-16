09620d041fdbf3556b3aa03c5abc3aacf095fbdd
Kevin Delahunt <kevin.delahunt@kevd.design>
2025-05-16 11:10:03 -0400

    Refactor Hamburger component to update SVG dimensions and improve visual consistency.

--- src/app/(site)/elements/Hamburger.tsx
+++ src/app/(site)/elements/Hamburger.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react'
 
 const Hamburger: FC = () => {
   return (
-    <svg width="58" height="49" viewBox="0 0 58 49" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M0 5.5C0 2.46243 2.46243 0 5.5 0H52.5C55.5376 0 58 2.46243 58 5.5C58 8.53757 55.5376 11 52.5 11H5.5C2.46243 11 0 8.53757 0 5.5Z" className="fill-emerald-800"/>
-      <rect className="fill-emerald-800" y="38" width="58" height="11" rx="5.5"/>
-      <rect className="fill-emerald-800" y="19" width="58" height="11" rx="5.5"/>
+    <svg width="48" height="33" viewBox="0 0 48 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 3.5C0 1.567 1.567 -3.8147e-06 3.5 -3.8147e-06H44.5C46.433 -3.8147e-06 48 1.567 48 3.5V3.5C48 5.43299 46.433 7 44.5 7H3.5C1.567 7 0 5.43299 0 3.5V3.5Z" className="fill-emerald-800"/>
+      <rect className="fill-emerald-800" y="13" width="48" height="7" rx="3.5"/>
+      <rect className="fill-emerald-800" y="26" width="48" height="7" rx="3.5"/>
     </svg>
   )
 }
 
 export default Hamburger
+
+
