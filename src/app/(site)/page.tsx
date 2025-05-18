89924b17e388486e3e9d85cde2b8d3d60b6b1313
Charles K. Delahunt <kevin.delahunt@kevd.design>
2025-05-18 15:29:25 -0400

    Refactor Home component: streamline data fetching and remove unused CTA logic

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,6 +1,8 @@
-import Cta from "./components/Cta";
+// import Cta from "./components/common/Cta";
 import { defineQuery } from "next-sanity";
 import { sanityFetch } from "../../sanity/lib/live";
+import { Hero } from './components/Hero'
+import { QUERY_HOME } from './queries/homeQuery';
 
 
 
@@ -19,7 +21,11 @@ const QUERY_CTAs = defineQuery(`*[_type == "siteSettingsSingleton"][0]{
     "toProjectSlug": toProject->projectSlug.current,
    
   }
-}`); 
+}`);
+
+
+
+
 
 
 
@@ -27,65 +33,64 @@ const QUERY_CTAs = defineQuery(`*[_type == "siteSettingsSingleton"][0]{
 export default async function Home() {
 
 
-  // Fetch the data from Sanity
-  const resCTAs  = await sanityFetch({query: QUERY_CTAs,});
+    // Fetch the data from Sanity
+  const resCTAs = await sanityFetch({ query: QUERY_CTAs });
+  const resHome = await sanityFetch({ query: QUERY_HOME });
 
-
- 
- 
-  // Check if the CTA data is available
-  if (!resCTAs || !resCTAs.data) {
-    console.error("No data found in the response:", resCTAs);
-    return <div>No CTA data available</div>;
+  // Check if both data sets are available
+  if (!resCTAs?.data || !resHome?.data) {
+    console.error("Missing data in response:", { resCTAs, resHome });
+    return <div>No data available</div>;
   }
 
-  
-  const dataCTA = resCTAs.data; 
+  // const dataCTA = resCTAs.data;
+  const { hero } = resHome.data;
+
+  console.log("hero data", hero);
 
   
-  // Check if the data is empty or undefined
-
-  if (!dataCTA) {
-    return <div>No CTA data available</div>;
-  }
-  
 
 
-  // Destructure the data to get the CTAs
+//   // Destructure the data to get the CTAs
 
-  const { reviewCTA, heroCTA, projectCTA,  viewReviewsCTA, submitReviewCTA, servicesCTA } = dataCTA;
-
-
-  const arrayOfAllCTAs = [
-    reviewCTA,
-    heroCTA,
-    projectCTA,
-    viewReviewsCTA,
-    submitReviewCTA,
-    servicesCTA
-  ];
-
-  const normalizedCTAs = arrayOfAllCTAs.map((cta) => {
-  if (!cta) return null;
-  return {
-    label: cta.linkLabel ?? undefined,
-    linkType: cta.linkType ?? undefined,
-    toPage: 'toPage' in cta ? cta.toPage ?? undefined : undefined,
-    externalLink: 'externalLink' in cta ? cta.externalLink ?? undefined : undefined,
-    toProjectSlug: 'toProjectSlug' in cta ? cta.toProjectSlug ?? undefined : undefined,
-  };
-});
+//   const { reviewCTA, heroCTA, projectCTA,  viewReviewsCTA, submitReviewCTA, servicesCTA } = dataCTA;
 
 
+//   const arrayOfAllCTAs = [
+//     reviewCTA,
+//     heroCTA,
+//     projectCTA,
+//     viewReviewsCTA,
+//     submitReviewCTA,
+//     servicesCTA
+//   ];
 
+//   const normalizedCTAs = arrayOfAllCTAs.map((cta) => {
+//   if (!cta) return null;
+//   return {
+//     label: cta.linkLabel ?? undefined,
+//     linkType: cta.linkType ?? undefined,
+//     toPage: 'toPage' in cta ? cta.toPage ?? undefined : undefined,
+//     externalLink: 'externalLink' in cta ? cta.externalLink ?? undefined : undefined,
+//     toProjectSlug: 'toProjectSlug' in cta ? cta.toProjectSlug ?? undefined : undefined,
+//   };
+// });
 
+if (!hero.image) return null;
 
   return (
-  <div>
-    {normalizedCTAs.map((cta, index) =>
-      cta ? <Cta key={index} {...cta} /> : null
-    )}
-  </div>
+
+    <main>
+      <Hero
+        image={hero.image}
+        headline={hero.headline}
+        cta={hero.cta}
+      />
+            {/* {normalizedCTAs.map((cta, index) =>
+        cta ? <Cta key={index} {...cta} /> : null
+      )} */}
+    </main>
+
   );
 }
 
